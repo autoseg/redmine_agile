9b66dbfb26f43dff6e9bb20cac6225464cf481eb
Kássio Borges <kassioborgesm@gmail.com>
2012-11-18 08:43:23 -0200

    show closed issues, refactoring QueryPatch

--- lib/prioritize/query_patch.rb
+++ lib/prioritize/query_patch.rb
@@ -1,9 +1,13 @@
 require_dependency 'query'
 
 module Prioritize::QueryPatch
+  unloadable
+
   def self.extended(base)
+    column = "#{Issue.table_name}.prioritization"
+
     query_column = QueryColumn.new(:prioritization,
-                                   :sortable => "#{Issue.table_name}.prioritization",
+                                   :sortable => column,
                                    :groupable => false)
 
     base.add_available_column(query_column)
