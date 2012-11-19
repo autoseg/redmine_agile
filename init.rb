242c974d45d49a772e400f9170500fa2f93b9fc4
Kássio Borges <kassioborgesm@gmail.com>
2012-11-19 11:25:09 -0200

    fix configs with plugin name

--- init.rb
+++ init.rb
@@ -1,13 +1,13 @@
 require_dependency 'prioritize/issue_patch'
 require_dependency 'prioritize/query_patch'
 
-Redmine::Plugin.register :prioritize do
+Redmine::Plugin.register :redmine_prioritize do
   name 'Prioritize plugin'
   author 'Kassio Borges & Brian Storti'
   description 'Plugin that give real order to issues'
   version '0.0.1'
-  url 'https://github.com/kassio/redmine-prioritize'
-  author_url 'https://github.com/kassio/redmine-prioritize#readme'
+  url 'https://github.com/kassio/redmine_prioritize'
+  author_url 'https://github.com/kassio/redmine_prioritize#readme'
 
   permission :prioritization,
     { :prioritization => [:index, :update] },
